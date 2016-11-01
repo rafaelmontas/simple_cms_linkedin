9a80701b3571820a04901b5b12710db738637ade
Rafael Montas <rafaelmontas1@gmail.com>
2016-11-01 16:41:36 -0400

    CRUD, new, create actions

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,9 +9,20 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => "Default"})
   end
 
   def create
+    #instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    #Save the object
+    if @subject.save
+      #if save succeeds, redirect to index action
+      redirect_to (subjects_path)
+    else
+    #if save fails, redisplay the form so users can fix problems
+      render ("new")
+    end
   end
 
   def edit
@@ -24,5 +35,10 @@ class SubjectsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
