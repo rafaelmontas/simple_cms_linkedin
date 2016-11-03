12d5b76d26abd0e6326c37450cb4a4159524bbd6
Rafael Montas <rafaelmontas1@gmail.com>
2016-11-03 16:45:27 -0400

    Challenge completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-
   root 'demo#index'
 
+  resources :sections do
+    member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+  
   resources :subjects do
     member do
       get :delete
